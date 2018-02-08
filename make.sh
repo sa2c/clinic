aaff71fb7de6efe80b270c8313d5601cd2788b62
SA2C WWW <sa2c-support@swansea.ac.uk>
2018-02-08 13:24:04 +0000

    detect python version, create venv if necessary

--- make.sh
+++ make.sh
@@ -1,3 +1,22 @@
-source clinic/bin/activate
-python3 ./generate.py clinics site/index.html --annual_template=annual_template.html
-python3 ./rooms.py rooms site
+if [ -x "$(command -v python3)" ]
+then
+    PYTHON=python3
+elif [ -x "$(command -v python36)" ]
+then
+    PYTHON=python36
+else
+    echo Python 3 not found
+    exit 1
+fi
+
+if [ ! -f "clinic/bin/activate" ]
+then
+    $PYTHON -m venv clinic
+    source clinic/bin/activate
+    pip install -r requirements.txt
+else
+    source clinic/bin/activate
+fi
+    
+$PYTHON ./generate.py clinics site/index.html --annual_template=annual_template.html
+$PYTHON ./rooms.py rooms site
